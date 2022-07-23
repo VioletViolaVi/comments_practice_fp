931c8f8fb4e8879d277d726a072d32c3fe5c67a9
vivian <vivian.v18723@gmail.com>
2022-07-23 23:34:17 +0100

    add fs.unlink()

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const commentsArr = require("./commentsData");
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 // const cors = require("cors");
 // const http = require("http");
 // app.use(express.json());
@@ -16,10 +17,21 @@ app.get("/comments", (req, res) => {
   res.send(commentsArr);
 });
 
-fs.readFile("./commentsData.js", "utf8", (err, data) => {
+fs.readFile(path.join(__dirname, "commentsData.js"), "utf8", (err, data) => {
   if (err) throw err;
-  console.log(data);
+  console.log("1st: ", data);
 });
+
+fs.unlink(path.join(__dirname, "draft.js"), (err) => {
+  if (err) throw err;
+  console.log("1st: ", err);
+});
+
+// // continuous w/ nodemon :(
+// fs.appendFile(path.join(__dirname, "draft.js"), 'console.log("this is a draft");', (err) => {
+//   if (err) throw err;
+//   console.log("2nd: ", "been written!");
+// });
 
 const port = 4000;
 app.listen(port, () => {
