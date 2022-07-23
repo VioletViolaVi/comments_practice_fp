0e415b29cfdbb1790850733138ac78f10f166219
vivian <vivian.v18723@gmail.com>
2022-07-23 13:21:33 +0100

    add appendFile()

--- index.js
+++ index.js
@@ -8,18 +8,27 @@ fs.readFile(
   "utf8",
   (err, data) => {
     if (err) throw err;
-    console.log(data);
+    console.log("1st: ", data);
   }
 );
 
-console.log("hello there!");
+console.log("2nd: ", "hello there!");
 
 fs.writeFile(
-  path.join(__dirname, "text_files", "greeting_reply.txt"),
-  "Hi and good day to you too!",
+  path.join(__dirname, "text_files", "written_file.txt"),
+  "The Written File!",
   (err) => {
     if (err) throw err;
-    console.log("Da file has been written!");
+    console.log("3rd: ", "been written!");
+  }
+);
+
+fs.appendFile(
+  path.join(__dirname, "text_files", "appended_file.txt"),
+  "The Appended File!",
+  (err) => {
+    if (err) throw err;
+    console.log("4th: ", "been appended!");
   }
 );
 
