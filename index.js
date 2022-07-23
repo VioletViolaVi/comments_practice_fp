80b601ffd22e70480b0655d9a6e32720898e86dc
vivian <vivian.v18723@gmail.com>
2022-07-23 13:08:17 +0100

    add writeFile() method

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const fs = require("fs");
+const path = require("path");
 
 // 'utf8' makes .txt content in string
-fs.readFile("./files/greeting.txt", "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+// < (path.join(__dirname, "text_files", "greeting.txt") > same as "./text_files/greeting.txt"
+fs.readFile(
+  path.join(__dirname, "text_files", "greeting.txt"),
+  "utf8",
+  (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  }
+);
+
+console.log("hello there!");
+
+fs.writeFile(
+  path.join(__dirname, "text_files", "greeting_reply.txt"),
+  "Hi and good day to you too!",
+  (err) => {
+    if (err) throw err;
+    console.log("Da file has been written!");
+  }
+);
 
 // catches uncaught errors
 process.on("uncaughtException", (err) => {
