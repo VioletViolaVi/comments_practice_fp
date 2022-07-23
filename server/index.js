08b3570ea5fceee4bc524082a4c32fd4dd5cc401
vivian <vivian.v18723@gmail.com>
2022-07-23 20:26:42 +0100

    import cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const commentsArr = require("./commentsData");
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+// find out about these...
+app.use(express.json());
+app.use(cors());
+//
 
 app.get("/", (req, res) => {
   res.send("Welcome to the blog");
