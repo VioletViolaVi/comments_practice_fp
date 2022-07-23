cb290564d6022938eab9325754c4f15c95f15329
vivian <vivian.v18723@gmail.com>
2022-07-23 23:57:36 +0100

    add matching code to html

--- server/index.js
+++ server/index.js
@@ -17,15 +17,59 @@ app.get("/comments", (req, res) => {
   res.send(commentsArr);
 });
 
+app.get("/html_page", (req, res) => {
+  const html = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <script src="./client/js/scripts.js" defer></script>
+      <link rel="stylesheet" href="./client/css/styles.css" />
+      <title>Comments</title>
+    </head>
+    <body>
+      <section>
+        <h3>comments to appear & stay below:</h3>
+        <div>
+          <ul id="displayComments" style="
+          border: solid black 0.2rem;
+          height: 15rem;
+          width: 30.5rem;
+          overflow: hidden;
+          margin: 2rem 0;
+      "></ul>
+        </div>
+      </section>
+  
+      <form id="wholeForm">
+        <textarea
+          name="commentTextarea"
+          id="commentTextarea"
+          cols="70"
+          rows="10"
+          maxlength="30"
+          placeholder="put comments here"
+        ></textarea>
+        <br />
+        <input type="submit" value="post" />
+      </form>
+    </body>
+  </html>
+  `;
+  res.send(html);
+});
+
 fs.readFile(path.join(__dirname, "commentsData.js"), "utf8", (err, data) => {
   if (err) throw err;
   console.log("1st: ", data);
 });
 
-fs.unlink(path.join(__dirname, "draft.js"), (err) => {
-  if (err) throw err;
-  console.log("1st: ", err);
-});
+// // continuous w/ nodemon :(
+// fs.unlink(path.join(__dirname, "draft.js"), (err) => {
+//   if (err) throw err;
+//   console.log("1st: ", err);
+// });
 
 // // continuous w/ nodemon :(
 // fs.appendFile(path.join(__dirname, "draft.js"), 'console.log("this is a draft");', (err) => {
@@ -37,3 +81,5 @@ const port = 4000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
+
+// should local storage be used?
