25eaf17a8ee4789f9730fe2c7b64211718dc0aa5
vivian <vivian.v18723@gmail.com>
2022-07-24 20:32:27 +0100

    add more practice

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,6 +1,7 @@
 const commentsBox = document.getElementById("displayComments"); // where comments are displayed
 const commentTextarea = document.getElementById("commentTextarea"); // where to type comments
 const form = document.getElementById("wholeForm");
+let allCommentsArr = [];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,8 +11,25 @@ form.addEventListener("submit", (e) => {
   newCommentLi.innerText = commentTextarea.value;
   commentsBox.appendChild(newCommentLi);
 
-  console.log("newCommentLi: ", newCommentLi);
-  console.log("commentsBox: ", commentsBox);
+  allCommentsArr.push(newCommentLi.innerText);
+
+  // console.log("newCommentLi: ", newCommentLi);
+  // console.log("commentsBox: ", commentsBox);
+  console.log("allCommentsArr: ", allCommentsArr);
+  // console.log("fullComments: ", fullComments);
 
   commentTextarea.value = "";
 });
+
+localStorage.setItem("all comments", allCommentsArr);
+localStorage.getItem("all comments");
+console.log("here =>", localStorage.getItem("all comments"));
+
+function displayComments() {
+  for (let i = 0; i < allCommentsArr.length; i++) {
+    const newCommentLi = document.createElement("li");
+    newCommentLi.innerText = allCommentsArr[i].value;
+    commentsBox.appendChild(newCommentLi);
+  }
+}
+displayComments();
